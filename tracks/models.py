fcba1042a1d59b4440591766bebf6864d5ada416
gprok <gprok60@gmail.com>
2022-03-24 19:51:42 +0200

    initial commit with basic models for Sprint 1

---
+++ tracks/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=250)
+    release_date = models.DateField(null=True, blank=True)
+    genre = models.CharField(max_length=50)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=250)
+    is_verified = models.BooleanField(default=False)
+    about = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    duration = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT)
+    album = models.ForeignKey(Album, on_delete=models.RESTRICT)
+
